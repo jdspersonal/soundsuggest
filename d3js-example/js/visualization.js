782d2799978e6c0ef394fae9a26bb300a2f54692
JorisSchelfaut <joris.schelfaut@hotmail.com>
2012-12-26 19:11:09 +0100

    small updates

--- d3js-example/js/visualization.js
+++ d3js-example/js/visualization.js
@@ -19,29 +19,8 @@
  * </p>
  */
 
-var matrix5x5 = [
-    [0, 5, 7, 2, 0],
-    [5, 0, 3, 4, 1],
-    [7, 3, 0, 6, 3],
-    [2, 4, 6, 0, 8],
-    [0, 1, 3, 8, 0]
-];
-var range5 = ["#000000", "#33585e", "#957244", "#F26223", "#155420"];
-var range5_artists = ["Cloudkicker", "Placebo", "Deftones", "Nine Inch Nails", "Massive Attack"];
-
-var chord = d3.layout.chord()
-        .padding(.05)
-        .sortSubgroups(d3.descending)
-        .matrix(matrix5x5);
-
 var width = 550;
 var height = 550;
-var innerRadius = Math.min(width, height) * .41;
-var outerRadius = innerRadius * 1.1;
-
-var fill = d3.scale.ordinal()
-        .domain(d3.range(range5.length))
-        .range(range5);
 
 var svg = d3.select("#chart")
         .append("svg")
@@ -50,7 +29,29 @@ var svg = d3.select("#chart")
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var paths = svg.append("g")
+var matrix5x5 = [
+    [0, 5, 7, 2, 0],
+    [5, 0, 3, 4, 1],
+    [7, 3, 0, 6, 3],
+    [2, 4, 6, 0, 8],
+    [0, 1, 3, 8, 0]
+];
+var range5 = ["#000000", "#33585e", "#957244", "#F26223", "#155420"];
+
+var chord = d3.layout.chord()
+        .padding(.05)
+        .sortSubgroups(d3.descending)
+        .matrix(matrix5x5);
+
+var fill = d3.scale.ordinal()
+        .domain(d3.range(range5.length))
+        .range(range5);
+
+
+var innerRadius = Math.min(width, height) * .41;
+var outerRadius = innerRadius * 1.1;
+
+svg.append("g")
         .selectAll("path")
         .data(chord.groups)
         .enter().append("path")
@@ -63,6 +64,19 @@ var paths = svg.append("g")
         .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
         .on("mouseover", fade(.1))
         .on("mouseout", fade(1));
+
+svg.append("g")
+        .attr("class", "chord")
+        .selectAll("path")
+        .data(chord.chords)
+        .enter().append("path")
+        .style("fill", function(d) {
+            return fill(d.target.index);
+        })
+        .attr("d", d3.svg.chord().radius(innerRadius))
+        .style("opacity", 1);
+
+var range5_artists = ["Cloudkicker", "Placebo", "Deftones", "Nine Inch Nails", "Massive Attack"];
 
 svg.selectAll("text")
         .data(chord.groups)
@@ -83,17 +97,6 @@ svg.selectAll("text")
         })
         .on("mouseover", fade(.1))
         .on("mouseout", fade(1));
-
-svg.append("g")
-        .attr("class", "chord")
-        .selectAll("path")
-        .data(chord.chords)
-        .enter().append("path")
-        .style("fill", function(d) {
-            return fill(d.target.index);
-        })
-        .attr("d", d3.svg.chord().radius(innerRadius))
-        .style("opacity", 1);
 
 function fade(opacity) {
     return function(g, i) {
